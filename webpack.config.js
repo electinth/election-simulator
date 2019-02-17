e3e038c8bd671b60dea222df4e92cd648564f0c7
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-17 12:22:02 -0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const cssRules = process.env.NODE_ENV === 'production'
-  ? [{
-      loader: MiniCssExtractPlugin.loader,
-      options: {
-        // you can specify a publicPath here
-        // by default it use publicPath in webpackOptions.output
-        publicPath: './'
-      }
-    },
-    'css-loader'
-  ]
-  : ['style-loader', 'css-loader'];
+const cssRules =
+  process.env.NODE_ENV === "production"
+    ? [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            // you can specify a publicPath here
+            // by default it use publicPath in webpackOptions.output
+            publicPath: "./"
+          }
+        },
+        "css-loader"
+      ]
+    : ["style-loader", "css-loader"];
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html"
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
@@ -33,13 +35,13 @@ module.exports = {
     })
   ],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"]
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: 'babel-loader',
+        use: "babel-loader"
       },
       {
         test: /\.css$/,
@@ -47,23 +49,19 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ["file-loader"]
       },
       {
-        type: 'javascript/auto',
+        type: "javascript/auto",
         test: /\.(json)$/,
-        use: [
-          'file-loader'
-        ]
+        use: ["file-loader"]
       }
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "public"),
     compress: true,
     port: 9000,
-    open: 'Google Chrome',
+    open: "Google Chrome"
   }
 };
