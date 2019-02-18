0f505f6b252a1a809e8ea691ccea61ccf2c1e389
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-18 01:00:25 -0800

    update page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import './style-header.css';
-import './style.css';
+import './css/fonts.css';
+import './css/style-header-white.css';
+import './css/style.css';
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
-
-const propTypes = {
-  className: PropTypes.string,
-};
-const defaultProps = {
-  className: '',
-};
 
 class App extends React.Component {
   render() {
-    const { className } = this.props;
-
-    return <div className={className}>under construction</div>;
+    return (
+      <React.Fragment>
+        <section class="container">
+          <header class="col">
+            <h3>ขั้นที่ 1. สมมติว่าแต่ละพรรคได้ส.ส.เท่านี้</h3>
+          </header>
+          <div class="col">
+            under construction
+          </div>
+        </section>
+        <section class="container">
+          <header class="col">
+            <h3>ขั้นที่ 2. ลองจัดตั้งรัฐบาล</h3>
+            <p>
+              หลังจากการเลือกตั้งและนับคะแนน
+              ก็ถึงเวลาที่บรรดาพรรคการเมืองจะจับมือกัน
+              เพื่อเลือกนายกรัฐมนตรีและจัดตั้งรัฐบาล
+              ความเป็นไปได้มีมากมายหลายแบบ
+              ลองเลือกพรรคหลัก, พรรคร่วมรัฐบาล และ จำนวนเสียงที่ได้จากส.ว. ดู
+            </p>
+          </header>
+          <div class="col">
+            under construction
+          </div>
+        </section>
+      </React.Fragment>
+    );
   }
 }
-
-App.propTypes = propTypes;
-App.defaultProps = defaultProps;
 
 export default hot(module)(App);
