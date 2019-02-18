84a6cea50431065e9cd0494c89324ced86a81099
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-17 23:34:17 -0800

    fix minor linting

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,68 @@
 /* eslint-disable sort-keys */
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const cssRules =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === 'production'
     ? [
         {
           loader: MiniCssExtractPlugin.loader,
           options: {
             // you can specify a publicPath here
             // by default it use publicPath in webpackOptions.output
-            publicPath: "./"
-          }
+            publicPath: './',
+          },
         },
-        "css-loader"
+        'css-loader',
       ]
-    : ["style-loader", "css-loader"];
+    : ['style-loader', 'css-loader'];
 
 module.exports = {
-  entry: "./src/index.jsx",
+  entry: './src/index.jsx',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html"
+      template: 'src/index.html',
     }),
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: "babel-loader"
+        use: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: cssRules
+        use: cssRules,
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"]
+        use: ['file-loader'],
       },
       {
-        type: "javascript/auto",
+        type: 'javascript/auto',
         test: /\.(json)$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   devServer: {
-    contentBase: path.join(__dirname, "public"),
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
     port: 9000,
-    open: "Google Chrome"
-  }
+    open: 'Google Chrome',
+  },
 };
