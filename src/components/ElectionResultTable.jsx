1cc961066254e81c4ef081ff4b55425018c8a4e0
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-02-21 23:04:15 -0800

    add color badge

--- src/components/ElectionResultTable.jsx
+++ src/components/ElectionResultTable.jsx
@@ -15,11 +15,18 @@ class ElectionResultTable extends React.Component {
   render() {
     const { className, sortedParties } = this.props;
 
+    const R = 4;
+
     return (
       <table className={className}>
         <tbody>
           {sortedParties.map(p => (
             <tr key={p.party.name} className="table table-sm">
+              <td>
+                <svg width={R * 2 + 2} height={R * 2 + 2}>
+                  <circle cx={R} cy={R} r={R} fill={p.party.color} />
+                </svg>
+              </td>
               <td className="party-name">{p.party.name}</td>
               <td>{p.seats}</td>
             </tr>
